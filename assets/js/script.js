ff68405fc15c7b7191b304d6ca90b0ea11a5af0b
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-06 15:08:09 -0500

    added some html to receive the provided CSS styling

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// solve structure
+// solve local storage
+// store save...
+// solve color sheme
+// if save button is tied to a specific spot in local storage...
+        // follow that pattern
+// form.value
+// 
+
+
+// past
+// present
+// future
+
+
+// document.querySelector("textarea[data-hour="9"]")
