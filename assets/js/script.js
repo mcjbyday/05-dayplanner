e4d8a8334c4f05f17032a1ca46821082640c7451
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-06 15:25:55 -0500

    added date to top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,15 @@
 // 
 
 
-// past
-// present
-// future
+// set #currentDay as dateContainer, use Moment to set date
+let dateContainer = document.querySelector("#currentDay");
+var currentDate = moment().format("MMM Do, YYYY");
+dateContainer.innerText = currentDate;
 
 
 // document.querySelector("textarea[data-hour="9"]")
+
+
+// past
+// present
+// future
