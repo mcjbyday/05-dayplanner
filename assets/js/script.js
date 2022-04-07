f52a42a02d2a2076555383dc7f271c860f8d2aad
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-07 14:11:21 -0500

    fixed colors on textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,16 +35,17 @@ for (var i = hoursStart ; i < hoursEnd ; i++) {
         displayTime = i + " AM";
     }
     // initialize HTML formatting, using individual hour values, hourly IDs, and the particular hour relationship to decide on the block's color
-    var hourBlock = $(`<div class="row col-12" data-hour="${i}" id="${i}"><div class="time-block col-2 hour">${displayTime}</div><textarea class="col-9 ${relativeTimeState} description"></textarea><button class="saveBtn col-1"><i class="fas fa-save"></i></button></div>`);
+    var hourBlock = $(`<div class="row col-12" data-hour="${i}" id="${i}"><div class="time-block col-2 hour ">${displayTime}</div><textarea class="col-9 ${relativeTimeState} description"></textarea><button class="saveBtn col-1"><i class="fas fa-save"></i></button></div>`);
     //
     timeBlockContainerEl.append(hourBlock);
     // for a given block of id i, get the value of the description from local storage and set the value with that particular localstorage data 
     $("#" +i +" .description").val(localStorage.getItem(i));
 }
-// adjust value of elements
-var hourWindowStatus = $('div[data-hour=""]').val();
+// // adjust value of elements
+// var hourWindowStatus = $('div[data-hour=""]').val();
 
 $(document).on("click", ".saveBtn", function(){
+    //
     var note = $(this).siblings(".description").val();
     var hour = $(this).parent().attr("data-hour");
 
