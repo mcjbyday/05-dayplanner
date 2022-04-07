1365ab5a4ef50ff8eba60167eb0297b81c8c8cb2
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-07 13:39:29 -0500

    added comments, set start and end hour variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,15 @@
-// TO DO
-
-
-// DONE 
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-    // THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-    // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// 
-// WHEN I click into a timeblock
-    // THEN I can enter an event
-// WHEN I click the save button for that timeblock
-    // THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-    // THEN the saved events persist
-
-// solve structure
-// solve local storage
-// store save...
-// solve color sheme
-// if save button is tied to a specific spot in local storage...
-        // follow that pattern
-// form.value
-// 
-
-
 // set #currentDay as dateContainer, use Moment to set date
 let dateContainer = document.querySelector("#currentDay");
 var currentDate = moment().format("MMM Do, YYYY");
 dateContainer.innerText = currentDate;
 
 let timeBlockContainerEl = $('.container');
+// set agenda boundaries
+let hoursStart = 8;
+let hoursEnd = 19;
 
-
-// make timeblock elements
-for (var i = 8 ; i < 19 ; i++) {
+// create, style, and append timeblock elements
+for (var i = hoursStart ; i < hoursEnd ; i++) {
     var presentHour = moment().format("h");
     var relativeTimeState = "";
 
@@ -59,10 +34,10 @@ for (var i = 8 ; i < 19 ; i++) {
     else if (i < 12) {
         displayTime = i + " AM";
     }
-    var hourWindow = $(`<div class="row col-12" data-hour="${i}" id="${i}"><div class="time-block col-2 hour">${displayTime}</div><textarea class="col-9 ${relativeTimeState} description"></textarea><button class="saveBtn col-1"><i class="fas fa-save"></i></button></div>`);
-    
-
-    timeBlockContainerEl.append(hourWindow);
+    // initialize HTML formatting, using individual hour values, hourly IDs, and the particular hour relationship to decide on the block's color
+    var hourBlock = $(`<div class="row col-12" data-hour="${i}" id="${i}"><div class="time-block col-2 hour">${displayTime}</div><textarea class="col-9 ${relativeTimeState} description"></textarea><button class="saveBtn col-1"><i class="fas fa-save"></i></button></div>`);
+    //
+    timeBlockContainerEl.append(hourBlock);
 
     $("#" +i +" .description").val(localStorage.getItem(i));
 }
@@ -75,8 +50,3 @@ $(document).on("click", ".saveBtn", function(){
 
     localStorage.setItem(hour, note);
 })
-
-// $("#8 .description").val(localStorage.getItem(8));
-
-// document.querySelector("textarea[data-hour="9"]")
-
